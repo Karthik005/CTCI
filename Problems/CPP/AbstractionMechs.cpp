0548e8badda860182b3a7b9d317195295b7a02ff
Karthik S <karthikbharadwaj005@gmail.com>
2019-03-06 19:50:53 +0530

    Add abstaction mechs more info

--- Problems/CPP/AbstractionMechs.cpp
+++ Problems/CPP/AbstractionMechs.cpp
@@ -102,3 +102,25 @@ class Vector_container : public Container
  * Done at runtime using virtual function table or simply the vtbl
  * Has mapping between class and function to call
  */
+
+/* Use unique_ptr to ensure that ptr gets destroyed once it goes out of scope
+ * Especially if using in containers
+ */
+unique_ptr<Shape> read_shape(istream &is) // read shape descriptions from input stream is
+{
+    // read shape header from is and find its Kind k
+    switch (k)
+    {
+    case Kind::circle:
+        // read circle data {Point,int} into p and r
+        return unique_ptr<Shape>{new Circle{p, r}}; // §5.2.1
+        // ...
+    }
+    void user()
+    {
+        vector<unique_ptr<Shape>> v;
+        while (cin)
+            v.push_back(read_shape(cin));
+        draw_all(v);       // call draw() for each element
+        rotate_all(v, 45); //call rotate(45) for each element
+    }                      // all Shapes implicitly destroyed
