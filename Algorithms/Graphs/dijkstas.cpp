b7c433e47a5af134b5d88002b7efd279d8fc119f
Karthik S <karthikbharadwaj005@gmail.com>
2019-04-07 22:35:08 +0530

    Add Graph

--- Algorithms/Graphs/dijkstas.cpp
+++ Algorithms/Graphs/dijkstas.cpp
@@ -1,5 +1,50 @@
 #include "../DataStructures/Graph.cpp"
 
 #include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <queue>
+#include <functional>
+#include <limits>
+#include <stdexcept>
 
 using namespace std;
+
+
+template<typename T>
+vector<int> djikstras(Graph<T> graph, g_node<T>* src) {
+    typedef g_node<T> gnd;
+
+    if (graph.size() == 0) {throw out_of_range("Graph is empty")};
+    unordered_set<T> seen_nodes;
+    unordered_map<gnd*, int> node_dist;
+    auto cmd = [&](gnd* a, gnd* b) {
+        return node_dist[b] - node_dist[a];
+    };
+    priority_queue<gnd*, vector<gnd*>, cmd> p_que;
+    for (int i = 0; i<graph.size() ; ++i) {
+        node_dist[graph[i]] = INT8_MAX;
+    }
+
+    node_dist[src] = 0;
+
+    for (int i = 0; i < graph.size(); ++i)
+    {
+        p_que.push(graph[i]);
+    }
+
+    gnd current_node = p_que.top();
+    while (!p_que.empty()) {
+        current_node = p_que.top();
+        for (const v_pair& x : current_node.adj_list()) {
+            if (seen_nodes.find(x.vert) == seen_nodes.end() &&
+                node_dist[current_node] + x.dist < node_dist[x.vert]) {
+                node_dist[x.vert] = node_dist[current_node] + x.dist;
+            }
+        }
+        p_que.pop();
+        
+    }
+}
--- DataStructures/Graph.cpp
+++ DataStructures/Graph.cpp
@@ -2,18 +2,23 @@
 
 using namespace std;
 
+template <typename T> struct v_pair;
 
 template<typename T>
 struct g_node {
     T value;
-    vector<g_node<T> *> adj_list;
-    vector<int> w_list;
+    vector<v_pair<T>> adj_list;
 
     void push_node(g_node * neighbr, int weight=1) {
-        adj_list.push_back(neighbr);
-        w_list.push_back(weight);
+        adj_list.push_back(v_pair<T>{neighbr, weight});
     }
 
+};
+
+template<typename T>
+struct v_pair {
+    g_node<T>* vert;
+    int dist;
 };
 
 template<typename T>
@@ -23,14 +28,26 @@ class Graph {
     
     public:
     
-    g_node * create_node(T value) {
-        g_node * ret_node = new g_node<T>();
+    g_node<T> * create_node(T value) {
+        g_node<T> * ret_node = new g_node<T>();
         ret_node->value = value;
         return ret_node;
     }
 
-    void add_node(g_node * node) {
+    void add_node(g_node<T> * node) {
         nodes.push_back(node);
+    }
+
+    g_node<T> * at(int index) {
+        return nodes[index];
+    }
+
+    g_node<T> operator[](int index){
+        return nodes[index];
+    }
+
+    int size() {
+        return nodes.size();
     }
 
     ~Graph() {
@@ -38,4 +55,4 @@ class Graph {
             delete x;
         }
     }
-}
+};
